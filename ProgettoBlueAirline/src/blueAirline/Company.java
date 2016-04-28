69350d741b1271652ec708d769be59112cf95598
Michele <Michele@10.65.1.183>
2016-04-28 10:20:51 +0200

    citys->cities

--- ProgettoBlueAirline/src/blueAirline/Company.java
+++ ProgettoBlueAirline/src/blueAirline/Company.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package blueAirline;
+package blueAirline;  //ciao a tutti.
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
