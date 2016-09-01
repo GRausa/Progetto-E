7814ad536103327b90f6fabaa87415c5c8f8959b
Zodraccir <zichittella.riccardo@gmail.com>
2016-09-01 17:33:56 +0200

    refactoring

--- BlueAirline/src/mail/Email.java
+++ BlueAirline/src/mail/Email.java
@@ -19,65 +19,64 @@ import javax.mail.internet.MimeMessage;
  */
 public class Email {
 
-    String destinatario; // questa variabile contiene l'indirizzo del destinatario
-    private static final String mittente = "thetablueairline@gmail.com"; // questa variabile contiene l'indirizzo del mittente
+    private String receiver; // questa variabile contiene l'indirizzo del destinatario
+    private static final String sender = "thetablueairline@gmail.com"; // questa variabile contiene l'indirizzo del mittente
     private static final String host = "smtp.gmail.com";                  // questa variabile contiene l'indirizzo dell'host per l'invio email del nostro.. 
     private static final String pass = "ipuffivolanti";
     //..gestore dell'alds , è da cambiare a seconda del gestore che si ha
 
-    String oggetto;          // questa variabile contiene l'oggetto dell'email    
-    String testo;     // questa variabile contiene il testo dell'email
+    private String object;          // questa variabile contiene l'oggetto dell'email    
+    private String textemail;     // questa variabile contiene il testo dell'email
 
     public Email(String sender, String object, String text) {
-        this.destinatario = sender;
-        this.oggetto = object;
-        this.testo = text;
+        this.receiver = sender;
+        this.object = object;
+        this.textemail = text;
     }
 
-    public String getDestinatario() {
-        return destinatario;
+    public String getReceiver() {
+        return receiver;
     }
 
-    public String getMittente() {
-        return mittente;
+    public String getSender() {
+        return sender;
     }
 
     public String getHost() {
         return host;
     }
 
-    public String getOggetto() {
-        return oggetto;
+    public String getObject() {
+        return object;
     }
 
-    public String getTesto() {
-        return testo;
+    public String getTextemail() {
+        return textemail;
     }
-    
 
     public static void sendMail(Email e) {
         try {
             Properties props = System.getProperties();
             props.put("mail.smtp.starttls.enable", "true");
             props.put("mail.smtp.host", Email.host);
-            props.put("mail.smtp.user", Email.mittente);
+            props.put("mail.smtp.user", Email.sender);
             props.put("mail.smtp.password", Email.pass);
             props.put("mail.smtp.port", "587");
             props.put("mail.smtp.auth", "true");
             props.put("mail.debug", "true");
-            
-            Session session = Session.getInstance(props, new GMailAuthenticator(Email.mittente, Email.pass));
+
+            Session session = Session.getInstance(props, new GMailAuthenticator(Email.sender, Email.pass));
             MimeMessage message = new MimeMessage(session);
-            Address fromAddress = new InternetAddress(Email.mittente);
-            Address toAddress = new InternetAddress(e.getDestinatario());
-            
+            Address fromAddress = new InternetAddress(Email.sender);
+            Address toAddress = new InternetAddress(e.getReceiver());
+
             message.setFrom(fromAddress);
             message.setRecipient(Message.RecipientType.TO, toAddress);
-            
-            message.setSubject(e.getOggetto());
-            message.setText(e.getTesto());
+
+            message.setSubject(e.getObject());
+            message.setText(e.getTextemail());
             Transport transport = session.getTransport("smtp");
-            transport.connect(host, mittente, pass);
+            transport.connect(host, sender, pass);
             message.saveChanges();
             Transport.send(message);
             transport.close();
